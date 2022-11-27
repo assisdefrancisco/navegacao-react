657478d0703f00a0441fba3a19f0958e945a68c2
Francisco de Assis <a124fr@gmail.com>
2022-11-27 16:11:20 -0300

    Passando paramentro na rota

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -7,11 +7,14 @@ import {
 
 import About from '../../views/examples/About'
 import Home from '../../views/examples/Home'
+import Param from '../../views/examples/Param';
 
 const Content = props => (
     <main className='Content'>
         <Routes>
             <Route path='/about' element={<About />} />
+            <Route path='/param/:id' element={<Param />} /> {/* :id -> representa o parametro, não é valor literal */}
+            {/* <Route path='/about' component={About} /> */}
             <Route path='/' element={<Home />} />
         </Routes>
     </main>
