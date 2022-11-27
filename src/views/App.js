ae698d06f024000191a88bd3a57fc119b5059074
Francisco de Assis <a124fr@gmail.com>
2022-11-27 15:13:01 -0300

    Estruturando navegação na aplicação

--- src/views/App.js
+++ src/views/App.js
@@ -3,10 +3,14 @@ import React from 'react';
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
+import { BrowserRouter } from 'react-router-dom';
+
 const App = props => (
     <div className='App'>
-        <Menu />
-        <Content />
+        <BrowserRouter>
+            <Menu />
+            <Content />
+        </BrowserRouter>
     </div>
 )
 
